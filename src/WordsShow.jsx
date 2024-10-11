da34abd6544d4c852f15a3a64f0bf8a0852f172e
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-10-11 08:24:18 -0400

    PUT WORD ON TOP because always first in the two lists anyways. Did temporary trim fix until can debug create_words in backend leaving a space after the occasional word.

--- src/WordsShow.jsx
+++ src/WordsShow.jsx
@@ -2,19 +2,24 @@ export function WordsShow({ verse, word }) {
   return (
     <div>
       <h1>Word information</h1>
+      <p>Text: {.trim()}</p>
       <p>Book: {verse.book}</p>
       <p>Chapter: {verse.chapter}</p>
       <p>Verse: {verse.verse_number}</p>
       <p>Word: {word.position}</p>
-      <p>Text: {word.text}</p>
+
+      <p>
+      Verse text: {word.verse.text}
+      </p>
 
       <h2>Matches</h2>
       <ul style={{ display: "grid", gridTemplateColumns: "1fr" }}>
         {word.matches.filter((match) => match.matched_word.id !== word.id).map((match) => (
           <li key={match.id} style={{ display: "flex", justifyContent: "flex-end" }}>
             <span>
-              <a href={`/words/${match.matched_word.id}`}>{match.matched_word.text}</a>
               {match.matched_word.verse.text}
+              ({match.matched_word.verse.book} {match.matched_word.verse.chapter}:{match.matched_word.verse.verse_number})
+              <a href={`/words/${match.matched_word.id}`}> &nbsp;{match.matched_word.text.trim()}</a>
             </span>
           </li>
         ))}
@@ -25,8 +30,9 @@ export function WordsShow({ verse, word }) {
         {word.roots.filter((root) => root.id !== word.id).map((root) => (
           <li key={root.id} style={{ display: "flex", justifyContent: "flex-end" }}>
             <span>
-              <a href={`/words/${root.id}`}>{root.text}</a>
               {root.verse.text}
+              ({root.verse.book} {root.verse.chapter}:{root.verse.verse_number})
+              <a href={`/words/${root.id}`}> &nbsp;{root.text.trim()}</a>
             </span>
           </li>
         ))}
